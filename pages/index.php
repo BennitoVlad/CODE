4849b58e38a511f16394bb3723252d2330db3c40
Revenge-of-Shadow <vyacheslav.pigida@gmail.com>
2024-03-26 21:04:09 +0200

    1 Minimal basic changes.

---
+++ pages/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Main page</title>
+        <link rel="stylesheet" href="../css/styles.css">
+    </head>
+    <body>
+
+    </body>
+
+</html>
+<?php
