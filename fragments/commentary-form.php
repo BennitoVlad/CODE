8a9d98ece7ab539923974f775217cee528a8db1f
Revenge-of-Shadow <vyacheslav.pigida@gmail.com>
2024-05-08 15:12:18 +0300

    E: Added commentary add and view fragments.

---
+++ fragments/commentary-form.php
@@ -0,0 +1,12 @@
+<form action="add-commentary-action.php" method="post" class="commentary">
+    <input id="participant-id" name="participant-id" type="hidden">
+    <div class="double-row">
+        <label for="participant" class="block-label-white">Користувач:</label>
+        <label id="participant" class="transparent-input">(Ви)</label>
+    </div>
+    <label for="message" class="block-label-white">Коментарій:</label>
+    <label></label>
+    <input id="message" class="transparent-input">
+    <label></label>
+    <input type="submit" value="Надіслати" class="block-label-blue">
+</form>
