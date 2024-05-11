c54c84728ea4432e733a842eb73b46e3c69a0cc2
Revenge-of-Shadow <vyacheslav.pigida@gmail.com>
2024-05-11 14:24:42 +0300

    K: I hate merging.

---
+++ connect/lang-ua.php
@@ -0,0 +1,55 @@
+<?php
+$MSG = Array(
+    'page_main' => "Головна сторінка",
+    'page_dashboard' => "Ваша сторінка",
+
+    'user_current' => "Активний користувач",
+    'user_profile' => "Ваш профіль",
+    'user_unknown' => "Ви не ввійшли в систему.",
+    'user_sign_in' => "Авторизуйтеся",
+    'user_sign_or' => "або",
+    'user_sign_up' => "зареєструйтеся",
+    'user_log_out' => "Вийти з аккаунта.",
+
+    'user' => "Користувач",
+    'you' => "Ви",
+    'client' => "Замовник",
+
+    'project' => "Проєкт",
+    'project_name' => "Назва проєкту",
+    'project_desc' => "Опис проєкту",
+    'project_type' => "Тип проєкту",
+    'project_reward' => "Оплата",
+    'project_date' => "Дата",
+    'project_completed' => "Завершений",
+
+    'project_post' => "Опублікувати проект",
+    'projects_open' => "Ваші незавершені проекти",
+    'projects_completed' => "Ваші готові проекти",
+    'projects_none' => "Таких проектів ще немає",
+
+    'projects_your' => "Ваші проекти",
+    'projects_assigned' => "Доручені вам проекти",
+    'projects_offered' => "Ваші пропозиції",
+    'projects_all' => "Всі незавершені проекти",
+    'project_types' => "Типи проектів",
+    'project_types_all' => "Всі типи проектів",
+
+    'currency_uah' => "₴",
+
+    'contractor' => "Фрилансер",
+    'contractor_assigned' => "Призначений фрілансер",
+    'comment' => "Коментарій",
+    'comment_date' => "Дата",
+    'comment_send' => "Надіслати",
+
+    'action_post' => "Опублікувати",
+    'action_edit' => "Редагувати",
+    'action_assign' => "Призначити контракт",
+    'action_revoke' => "Скасувати призначення",
+    'action_delete' => "Видалити",
+
+    'yes' => "Так",
+    'no' => "Ні",
+);
+?>
